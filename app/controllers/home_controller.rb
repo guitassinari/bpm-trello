36a9ecb77630dc2c12baa9976db58033b20b3470
Guilherme Tassinari <guitassinari@gmail.com>
2020-06-28 15:26:56 -0300

    show board names on home screen

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,7 @@
 
 class HomeController < ApplicationController
   def index
-    text = 'Angela Merkel met Nicolas Sarkozy on January 25th in ' +
-   'Berlin to discuss a new austerity package. Sarkozy ' +
-   'looked pleased, but Merkel was dismayed.'
-
-    pipeline =  StanfordCoreNLP.load(:tokenize, :ssplit, :pos, :lemma, :parse, :ner, :dcoref)
-    text = StanfordCoreNLP::Annotation.new(text)
-    pipeline.annotate(text)
-    @sentences = text.get(:sentences)
+    @member = Trello::Member.find(:me)
+    @boards = @member.boards
   end
 end
